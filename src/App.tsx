0201df99cc68bf4b3d4b52213d5e0e1b44a8a1f0
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-07-05 15:03:57 +0330

    feat: added FormSplitBill component

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,7 @@ function App(): JSX.Element {
         <FormAddFriend/>
         <Button>Add friend</Button>
       </div>
+      <FormSplitBill/>
     </div>
   )
 }
@@ -83,5 +84,24 @@ function FormAddFriend(): JSX.Element {
   )
 }
 
+function FormSplitBill(): JSX.Element {
+  return(
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>Bill value</label>
+      <input type="text" />
+      <label>Your expense</label>
+      <input type="text" />
+      <label>X's expense</label>
+      <input type="text" disabled/>
+      <label>Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split bill</Button>
+    </form>
+  )
+}
 
 export default App;

01ef1063822d02c8888ec25ce72ca683e821523e
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-07-05 18:07:46 +0330

    feat: implemented Adding friend to the friend list functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,66 @@
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
 
 const initialFriends = [
   {
-    id: 118836,
+    id: "118836",
     name: "Clark",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
-    id: 933372,
+    id: "933372",
     name: "Sarah",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
-    id: 499476,
+    id: "499476",
     name: "Anthony",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
 ];
 
-interface FriendProps {
-  friendObj: {
-    id: number;
+interface Friend {
+    id: string;
     name: string;
     image: string;
     balance: number;
-  };
+}
+
+interface FriendProps {
+  friendObj: Friend
 }
 
 interface ButtonProps {
   children: React.ReactNode;
   onClick: () => void;
 }
-
+interface FriendsListProps {
+  friends: Friend[]
+}
+interface FormAddFriendProps {
+  onAddFriend: (friend: Friend) => void
+}
 function App(): JSX.Element {
-  const [showFormFriend, setShowFormFriend] = useState(false);
+  const [showFormFriend, setShowFormFriend] = useState<boolean>(false);
+  const [friends, setFriends] = useState<Friend[]>(initialFriends)
 
   //show add friend form
   function handleShowForm(): void {
     setShowFormFriend(show => !show)
   }
 
+  // add friend function
+  function handleAddFreind(friend: Friend) {
+    setFriends(friends => [...friends , friend])
+    setShowFormFriend(false)
+  }
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {showFormFriend && <FormAddFriend/>}
+        <FriendsList friends={friends} />
+        {showFormFriend && <FormAddFriend onAddFriend={handleAddFreind}/>}
         <Button onClick={handleShowForm}>{showFormFriend ? "close" : "Add friend"}</Button>
       </div>
       <FormSplitBill />
@@ -55,10 +68,10 @@ function App(): JSX.Element {
   );
 }
 
-function FriendsList(): JSX.Element {
+function FriendsList({friends}: FriendsListProps): JSX.Element {
   return (
     <ul>
-      {initialFriends.map((friend) => (
+      {friends.map((friend) => (
         <Friend friendObj={friend} key={friend.id} />
       ))}
     </ul>
@@ -90,14 +103,27 @@ function Button({ onClick , children }: ButtonProps): JSX.Element {
   return <button className="button" onClick={onClick}>{children}</button>;
 }
 
-function FormAddFriend(): JSX.Element {
+function FormAddFriend({onAddFriend}: FormAddFriendProps): JSX.Element {
+  const [friendName , setAddFriendName] = useState("")
+  const [imageUrl , setImageUrl] = useState("https://i.pravatar.cc/48")
+
+  // submit form add friend
+  function handleSubmit(e: SyntheticEvent): void {
+    const id = crypto.randomUUID()
+    e.preventDefault()
+    if(!friendName || !imageUrl) return
+    const friendObj = {id, name: friendName, image: `${imageUrl}?=${id}` , balance: 0}
+    setAddFriendName("")
+    setImageUrl("https://i.pravatar.cc/48")
+    onAddFriend(friendObj)
+  }
   return ( 
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
     <label>Friend name</label>
-    <input type="text" />
+    <input type="text" value={friendName} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setAddFriendName(e.target.value)}/>
 
     <label>Image URL</label>
-    <input type="text" />
+    <input type="text" value={imageUrl} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setImageUrl(e.target.value)}/>
     <Button>ADD</Button>
   </form>
 )
